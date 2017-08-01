5687a90f5a4cf6554000c1317ea53c1fe11eec56
leocavalcante <lc@leocavalcante.com>
2017-08-01 17:01:40 -0300

    Add some iOS code

--- index.ios.js
+++ index.ios.js
@@ -1,53 +1,30 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
+import React from 'react'
+import { AppRegistry, DeviceEventEmitter } from 'react-native'
+import Beacons from 'react-native-beacons-manager'
 
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+export default class HelloBeacons extends React.Component {
+  componentDidMount() {
+    const region = {
+      identifier: 'Estimotes'
+    }
 
-export default class HelloBeacons extends Component {
+    Beacons.requestWhenInUseAuthorization()
+    Beacons.startMonitoringForRegion(region)
+    Beacons.startRangingBeaconsInRegion(region)
+    Beacons.startUpdatingLocation()
+
+    DeviceEventEmitter.addListener('beaconsDidRange', data => this.setState({ data }))
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
+      <View style={{ flex: 1 }}>
+        <Text style={{ flex: 1 }}>
+          {JSON.stringify(this.state.data)}
         </Text>
       </View>
-    );
+    )
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('HelloBeacons', () => HelloBeacons);
+AppRegistry.registerComponent('HelloBeacons', () => HelloBeacons)
